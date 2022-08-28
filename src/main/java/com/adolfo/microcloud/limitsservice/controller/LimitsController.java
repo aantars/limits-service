a74a6d4aa15fd64042e66696a88e670cc32cc8db
Adolfo <aantars@hotmail.com>
2022-08-28 12:49:12 +0200

    starting externalizing config (not working yet)

--- src/main/java/com/adolfo/microcloud/limitsservice/controller/LimitsController.java
+++ src/main/java/com/adolfo/microcloud/limitsservice/controller/LimitsController.java
@@ -1,19 +1,23 @@
 package com.adolfo.microcloud.limitsservice.controller;
 
-import com.adolfo.microcloud.limitsservice.LimitsServiceApplication;
+import com.adolfo.microcloud.limitsservice.configuration.Configuration;
 import com.adolfo.microcloud.limitsservice.entity.Limits;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    Configuration configuration;
+
+
     @GetMapping("/limits")
     public Limits retrieveLimits() {
         // TODO:  connect with the service to get the info
+     //   int min = configuration.getMinimum();
+     //   Limits retrievedLimits = new Limits(configuration.getMinimum(), configuration.getMaximum());
         Limits retrievedLimits = new Limits(1,9);
         return retrievedLimits;
     }
